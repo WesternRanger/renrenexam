0c4df21d9eeacf309c15b84d59f1537f2d57a677
WesternRanger <western_ranger@163.com>
2015-01-25 16:08:07 +0800

    jQuery以前学的不精，就开始学原生脚本了。导致现在写的东西原生和jQuery混用了。现在好了，基本知道差异了。

--- js/jQueryNew.js
+++ js/jQueryNew.js
@@ -2,32 +2,75 @@
  * Created by Administrator on 2015/1/23.
  */
 $(function(){
-    //$("#ul li #one").attr('disabled',false);
-    var useMoney = 0,//屏幕要显示的可用余额
-        leftMoney;
-    var drinkCell = $("#ul li");
-    var drinkPrice = drinkCell.find("input");
-    var drinkBtn = drinkCell.find("button");
+    var DOC = $(document),
+        useMoney = 0,//屏幕要显示的可用余额
+        outHtml = '',//出货累加
 
-    $("#drop-money").on('click',function(e){
-        var target = e.target;
-        useMoney += parseInt(target.value);
-        //投币后屏幕显示余额);
-        $("#left-money").html(useMoney);
-        compare(useMoney)
-    });
+        drinkCellParent = $("#ul"),
+        drinkPrice = drinkCellParent.find("li input"),
+        drinkBtn = drinkCellParent.find("li button");
 
+
+    //购买
+    drop();
+
+    //退币；
+    back();
+
+    buy();//点击购买饮料
+
+    function drop(){
+        $("#drop-money").on('click',function(e){
+            var target = $(e.target);
+            useMoney += parseInt(target.val());
+            //投币后屏幕显示余额);
+            $("#left-money").html(useMoney);
+            compare(useMoney)
+        });
+    }
+
+    //退币，液晶屏重置
+    function back(){
+        $("#back").find("input").on("click",function(){
+            var leftMoney = $("#left-money").html();
+            $("#pocket").html(leftMoney);
+            $("#left-money").html("0");
+        })
+    }
+
+    //计算价格，点亮可购买的商品
     function compare(left){
-        for(var i in drinkPrice){
-            if(left >= drinkPrice[i].value){
-                var index = i;
-                console.log(drinkPrice[i]);
-                //console.log(i);
-                console.log(drinkBtn[i]);
-                console.log(drinkPrice[i].parentNode)
-                drinkBtn[index].attr('disabled',false);
+        drinkPrice.each(function(i){
+            if(left >= drinkPrice.eq(i).val()){
+                drinkBtn.eq(i).attr("disabled",false);
             }
-        }
+        })
+        //for(var i in drinkPrice){
+        //    if(left >= drinkPrice[i].value){
+        //        drinkBtn.eq(i).attr('disabled',false);
+        //        //点击商品购买按钮
+        //
+        //    }
+        //}
+    }
+
+    //点击商品购买按钮
+    function buy(){
+        drinkCellParent.on("click",function(e){
+            //获取事件对象；
+            var target = $(e.target);
+
+            var targetParent = target.parent();
+            //获取饮料名字
+            var targetName = targetParent.children(".drink-name").html();
+            //获取饮料图片地址
+            var targetSrc = targetParent.children("img").attr("src");
+
+            console.log(targetName);
+            console.log(targetSrc);
+            outHtml += targetName+'<br>'+'<img src="'+targetSrc+'"/>'+'<br>'
+            $("#out").html(outHtml);
+        })
     }
 
 })
